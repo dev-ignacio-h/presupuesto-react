0ae233c1223f8aee7752da7b060f3aacea76f3e3
Ignacio <dev.ignacio.h@gmail.com>
2021-03-20 10:58:50 -0300

    guardar gastos en state principal

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Error from './Error';
+import shortid from 'shortid';
 
-const Formulario = () => {
+const Formulario = ({ agregarNuevoGasto }) => {
   const [nombre, guardarNombre] = useState('');
   const [cantidad, guardarCantidad] = useState(0);
   const [error, guardarError] = useState(false);
@@ -15,8 +16,18 @@ const Formulario = () => {
       return;
     }
     // constuir el gasto
+    const gasto = {
+      nombre,
+      cantidad,
+      id: shortid.generate(),
+    };
+
+    console.log(gasto);
     // pasar el gasto al componente principal
+    agregarNuevoGasto(gasto);
     // resetear el form
+    guardarNombre('');
+    guardarCantidad(0);
   };
   return (
     <form onSubmit={agregarGasto}>
